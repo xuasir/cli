89630c1b1ad0eaeae7509b584c1ffe276c4ccb9b
guo.xu <xuguo@outlook.it>
2021-03-19 15:22:40 +0800

    chore: ts demo增加类型案例

--- packages/playground/ts/xus.config.ts
+++ packages/playground/ts/xus.config.ts
@@ -3,8 +3,17 @@ import { defineConfig } from '@xus/cli'
 export default defineConfig({
   libBuild: {
     target: ['es2015'],
-    formats: ['esm', 'cjs'],
+    formats: ['esm'],
     sourcemap: false,
-    minify: false
+    minify: false,
+    rollTypes: true,
+    rollupChain(rc) {
+      rc.input({
+        index: './src/index.ts',
+        di: './src/di.ts'
+      })
+      return rc
+    },
+    alwaysEmptyDistDir: true
   }
 })
