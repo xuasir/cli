5ec197b6c1928657ab73ea9c82d9add003c83383
guo.xu <xuguo@outlook.it>
2021-03-22 17:22:58 +0800

    release: publish packages: @xus/create-lib@0.3.2

--- packages/create-lib/src/template/ts-lib-lerna/_eslintrc.js
+++ packages/create-lib/src/template/ts-lib-lerna/_eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: [require.resolve('@xus/eslint-config')]
+  extends: [require.resolve('@xus/eslint-config')],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 0
+  }
 }
